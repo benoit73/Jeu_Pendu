01ccb7fb805023039df41e9551415a994b9d2b0e
Benoit Mathiez <benoit.mathiez@saintmichelannecy.fr>
2023-10-17 15:31:07 +0200

    Ajoutez des fichiers projet.

---
+++ benoit_mathiez_penduv2/MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace benoit_mathiez_penduv2
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private LaClass myLaClass;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            myLaClass = new LaClass();
+            myLaClass.ListDeMot();
+            Initialiser();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+            if (button.Background != Brushes.Red && button.Background != Brushes.Green && myLaClass.NbMort < 7 && myLaClass.Gagne != true)
+            {
+                string lettre = button.Content.ToString();
+                char c = lettre[0];
+                myLaClass.TesterLettre(c);
+
+                if (myLaClass.BonneLettre == true)
+                {
+                    button.Background = Brushes.Green;
+
+                }
+                else
+                {
+                    button.Background = Brushes.Red;
+                    int numImage = myLaClass.NbMort;
+                    string imagePath = $"C:\\Users\\SIO\\source\\repos\\benoit_mathiez_penduv2\\benoit_mathiez_penduv2\\Pendu\\{numImage}.png";
+                    BitmapImage bitmapImage = new BitmapImage(new Uri(imagePath));
+                    monImage.Source = bitmapImage;
+                }
+                TbMot.Text = myLaClass.MotCache;
+
+                if (myLaClass.Gagne == true)
+                {
+                    GagneJeu();
+                }
+
+                if (myLaClass.NbMort == 7)
+                {
+                    TbMessage.Text = "Tu as perdu !!";
+                }
+            }
+
+        }
+
+        private void BtnReset_Click(object sender, RoutedEventArgs e)
+        {
+            Initialiser();
+        }
+
+        private void Initialiser()
+        {
+            myLaClass.SelectMot();
+            TbMot.Text = myLaClass.MotCache;
+            monImage.Source = null;
+            TbMessage.Text = "";
+            string[] aplphabet = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+
+            foreach (string s in aplphabet )
+            {
+                string nomBtn = "Btn" + s;
+                Button lebouton = FindName(nomBtn) as Button;
+                lebouton.Background = Brushes.Gray;
+            }
+        }
+
+
+        private void GagneJeu()
+        {
+            TbMessage.Text = "Tu as gagné !";
+        }
+    }
+}
---
+++ benoit_mathiez_penduv2/class.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace benoit_mathiez_penduv2
+{
+   
+    public class LaClass
+    {
+        List<string> list = new List<string>();
+        public string Mot { get; set; }
+        public string MotCache { get; set; }
+        public bool Gagne { get; set; }
+        public int NbMort { get; set; }
+        public bool BonneLettre { get; set; }
+
+        public void ListDeMot()
+        {
+            StreamReader stream = new StreamReader("C:\\Users\\SIO\\source\\repos\\benoit_mathiez_penduv2\\benoit_mathiez_penduv2\\mots.txt");
+            string line;
+            while ((line = stream.ReadLine()) != null)
+            {
+                list.Add(line);
+            }
+        }
+
+        public void SelectMot()
+        {
+            int listLength = list.Count;
+            Random random = new Random();
+            int nombreAleatoire = random.Next(0, listLength); 
+
+            Mot = list[nombreAleatoire];
+            list.RemoveAt(nombreAleatoire);
+
+            string motCachee = new string('*', Mot.Length);
+            MotCache = motCachee;
+            Gagne = false;
+            NbMort = 0;
+        }
+
+        public void TesterLettre(char lettre)
+        {
+            BonneLettre = false;
+            char[] charArray = MotCache.ToCharArray();
+
+            for (int i = 0; i < Mot.Length; i++)
+            {
+                if (Mot[i] == lettre)
+                {
+                    charArray[i] = lettre;
+                    BonneLettre = true;
+                }
+            }
+            MotCache = new string(charArray);
+
+            if (MotCache.Contains("*") == false)
+            {
+                Gagne = true;
+            }
+
+            if (BonneLettre == false)
+            {
+                NbMort++;              
+            }
+
+
+        }
+
+        
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
